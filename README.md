21e791e250b3e7ff7c5b86de904274454ae11842
snekw <dev.snekw@gmail.com>
2020-12-01 15:17:13 +0200

    Update to LCJS 2.1.0

--- README.md
+++ README.md
@@ -1,1 +1,149 @@
+# 3D Simple Surface Mesh
+
+![3D Simple Surface Mesh](3dSimpleSurfaceMesh.png)
+
+This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
+
+LightningChart JS is entirely GPU accelerated and performance optimized charting library for presenting massive amounts of data. It offers an easy way of creating sophisticated and interactive charts and adding them to your website or web application.
+
+The demo can be used as an example or a seed project. Local execution requires the following steps:
+
+- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+- Open the project folder in a terminal:
+
+        npm install              # fetches dependencies
+        npm start                # builds an application and starts the development server
+
+- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+
+
+## Description
+
+This example shows a 3D Mesh surface with various different dynamic styles selectable from the User Interface.
+
+The Chart is rendered using SurfaceMeshSeries3D, a series type that renders a continous 3D mesh, with each individual data point configurable with its own color (which can also be looked up dynamically from an attached Color table).
+
+The Mesh geometry is defined by warping a data point matrix into XYZ coordinates.
+In this example, the imaginary matrixes *rows* are warped into an *angle* on the YX plane and *columns* into Z coordinate as well as a radius, forming a 3D tube.
+
+The Mesh in the example has 150*150 (22 500) data points.
+
+## SurfaceMeshSeries3D data format
+
+`SurfaceMeshSeries3D` stores 3 kinds of user modifiable data.
+Each type of data has its own invalidation method. All these methods can be called either with:
+- callback function.
+    * Called back for each data point of the mesh.
+    * Function then returns the value to be associated with that data point.
+
+- data matrix.
+    * Multidimensional array of data values.
+
+It is also possible to not invalidate the whole mesh, but only a section of it. This is done by appending a second parameter, which specifies the range of modification (`GridRangeOptions`).
+
+
+### Geometry map
+
+Each data point on the mesh is associated with a 3D coordinate (`Point3D`).
+
+Geometry coordinates are invalidated with `SurfaceMeshSeries3D.invalidateGeometryOnly`
+```typescript
+// Invalidate full meshes geometry coordinates by callback.
+SurfaceMeshSeries3D.invalidateGeometryOnly(( row, column, prev ) => ({
+    x: row,
+    y: column,
+    z: Math.pow(row + column * rowsAmount, 2) / (rowsAmount * columnsAmount)
+}))
+```
+
+### Look-Up-Value map
+
+Each data point on the mesh can be associated with a numeric look-up value (`Number`).
+
+This can be used to dynamically color each data point based on an attached Color look-up table (`LUT`).
+Note that coloring by the look-up values needs to be enabled by setting the Series' *FillStyle* as `PalettedFill`.
+
+Look-Up-Values are invalidated with  `SurfaceMeshSeries3D.invalidateValuesOnly`
+```typescript
+// Define Color Look-Up-Table.
+const lut = new LUT( {
+    steps: [
+        { value: 0, color: ColorRGBA( 0, 0, 0 ) },
+        { value: 50, color: ColorRGBA( 255, 0, 0 ) }
+    ],
+    interpolate: true
+} )
+
+// Invalidate Look-Up-Values by data matrix.
+SurfaceMeshSeries3D.invalidateValuesOnly(
+    [
+        // First row.
+        [0,0,0,0,0],
+        // Second row.
+        [10,10,10,10,10],
+        // and so forth...
+        [20,20,20,20,20],
+        [30,30,30,30,30],
+        [40,40,40,40,40],
+    ]
+)
+
+// Configure Series fill style as look up from table.
+SurfaceMeshSeries3D.setFillStyle( new PalettedFill({ lut }) )
+```
+
+### Color map
+
+Each data point on the mesh can be associated with a `Color`.
+
+This can be used to color each data point with its own Color.
+Note that coloring by the individual Colors needs to be enabled by setting the Series' *FillStyle* as `IndividualPointFill`.
+
+Color values are invalidated with  `SurfaceMeshSeries3D.invalidateColorsOnly`
+```typescript
+// Invalidate first row of Colors by callback.
+SurfaceMeshSeries3D.invalidateColorsOnly(
+    ( row, column, prev ) => new ColorHSV( column * 10 ),
+    {
+        row: { start: 0, end: 1 },
+        column: { start: 0, end: numberOfColumns }
+    }
+)
+
+// Configure Series fill style as individual colors.
+SurfaceMeshSeries3D.setFillStyle( new IndividualPointFill() )
+```
+
+
+## API Links
+
+* [3D chart]
+* [3D surface mesh series]
+* [Grid range options]
+
+
+## Support
+
+If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
+
+Official [API documentation][1] can be found on [Arction][2] website.
+
+If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
+
+If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+
+[0]: https://github.com/Arction/
+[1]: https://www.arction.com/lightningchart-js-api-documentation/
+[2]: https://www.arction.com
+[3]: https://stackoverflow.com/questions/tagged/lightningchart
+[4]: https://www.arction.com/support-services/
+
+© Arction Ltd 2009-2020. All rights reserved.
+
+
+[3D chart]: https://www.arction.com/lightningchart-js-api-documentation/v2.1.0/classes/chart3d.html
+[3D surface mesh series]: https://www.arction.com/lightningchart-js-api-documentation/v2.1.0/classes/surfacemeshseries3d.html
+[Grid range options]: https://www.arction.com/lightningchart-js-api-documentation/v2.1.0/interfaces/gridrangeoptions.html
 
---
+++ src/index.js
@@ -0,0 +1,268 @@
+/*
+ * LightningChartJS example that showcases LineSeries in a 3D Chart.
+ */
+// Import LightningChartJS
+const lcjs = require('@arction/lcjs')
+
+// Extract required parts from LightningChartJS.
+const {
+    lightningChart,
+    SurfaceSeriesTypes3D,
+    ColorHSV,
+    ColorRGBA,
+    IndividualPointFill,
+    PalettedFill,
+    SolidFill,
+    LUT,
+    UIOrigins,
+    UIBackgrounds,
+    UIElementBuilders,
+    UILayoutBuilders,
+    emptyFill,
+    Themes
+} = lcjs
+
+
+
+/**
+ * Data generator function for the example.
+ */
+function WaterDropGenerator(
+    sizeX,
+    sizeZ,
+    xPositionsNormalized,
+    zPositionsNormalized,
+    amplitudes,
+    offsetLevel,
+    volatility
+) {
+    function CalculateWavesAtPoint(
+        x,
+        z
+    ) {
+        let resultValue = 0
+        const iOscillatorCount = oscillators.length
+        for (let i = 0; i < iOscillatorCount; i++) {
+            const oscillator = oscillators[i]
+            const distX = x - oscillator.centerX
+            const distZ = z - oscillator.centerZ
+            const dist = Math.sqrt(distX * distX + distZ * distZ)
+            resultValue += oscillator.gain * oscillator.amplitude * Math.cos(dist * volatility) * Math.exp(-dist * 3.0)
+        }
+        return resultValue
+    }
+
+    const iOscCount = amplitudes.length
+    const oscillators = []
+
+    for (let iOsc = 0; iOsc < iOscCount; iOsc++) {
+        oscillators[iOsc] = {
+            amplitude: amplitudes[iOsc],
+            centerX: xPositionsNormalized[iOsc],
+            centerZ: zPositionsNormalized[iOsc],
+            gain: 1,
+            offsetY: 0
+        }
+    }
+
+    const result = Array.from(Array(sizeZ)).map(() => Array(sizeX))
+    const dTotalX = 1
+    const dTotalZ = 1
+    const stepX = (dTotalX / sizeX)
+    const stepZ = (dTotalZ / sizeZ)
+
+    for (let row = 0, z = 0; row < sizeZ; row++, z += stepZ) {
+        for (let col = 0, x = 0; col < sizeX; col++, x += stepX) {
+            result[col][row] = CalculateWavesAtPoint(x, z) + offsetLevel
+        }
+    }
+    return result
+}
+
+
+
+const chart3D = lightningChart().Chart3D( {
+    // theme: Themes.dark
+} )
+    .setTitle( 'Simple 3D Surface Mesh' )
+
+
+chart3D.getDefaultAxisX().setInterval( -3.55, 3.55 )
+chart3D.getDefaultAxisY().setInterval( -3.55, 3.55 )
+chart3D.getDefaultAxisZ().setInterval( -3.55, 3.55 )
+
+
+
+// Create color Look-Up-Tables for dynamic colouring.
+const paletteY = new LUT( {
+    steps: [
+        { value: -1.5, color: ColorRGBA( 0, 0, 255 ) },
+        { value: -0.2, color: ColorRGBA( 255, 255, 255 ) }
+    ],
+    interpolate: true
+} )
+const paletteValue = new LUT( {
+    steps: [
+        { value: 0, color: ColorRGBA( 0, 0, 0 ) },
+        { value: 30, color: ColorRGBA( 255, 255, 0 ) },
+        { value: 45, color: ColorRGBA( 255, 204, 0 ) },
+        { value: 60, color: ColorRGBA( 255, 128, 0 ) },
+        { value: 100, color: ColorRGBA( 255, 0, 0 ) }
+    ],
+    interpolate: true
+} )
+
+
+const rows = 150
+const columns = rows
+const surface = chart3D.addSurfaceSeries( {
+    type: SurfaceSeriesTypes3D.Mesh,
+    rows,
+    columns,
+    start: { x: 0, z: 0 },
+    end: { x: 100, z: 100 },
+    pixelate: true
+} )
+
+
+
+// Assign a Value to each coordinate of the Grid to be used when colouring by look up value.
+const waterdropData = WaterDropGenerator(
+    rows, // size of nodes in X
+    columns, // size of nodes in Z
+    [0.2, 0.5, 0.7], // Drop X positions in scale 0...1
+    [0.6, 0.5, 0.3], // Drop Z positions in scale 0...1
+    [23, 74, 16], // Amplitudes, as Y axis values
+    58, // Offset level (mid-Y)
+    25 // Volatility, wave generating density
+)
+surface.invalidateValuesOnly( ( row, column ) => waterdropData[row][column] )
+
+
+
+// Assign a Color to each coordinate of the Grid to be used when colouring by individual color.
+// Leave some blanks to showcase fall back color.
+surface.invalidateColorsOnly( ( row, column ) => Math.random() >= 0.80 ? ColorRGBA( 255, 0, 0 ) : undefined )
+
+
+
+// Define Mesh geometry as a function of:  (row, column) => { x, y, z }
+const y1 = ( t ) => .3 * Math.sin( t * 4 * Math.PI / columns )
+const y2 = ( t ) => 2.5 + Math.cos( t * 4 * Math.PI / columns )
+surface.invalidateGeometryOnly( ( row, column, prev ) => {
+    const angle = row * 2 * Math.PI / ( rows - 1 )
+    const radius = Math.abs( y2( column ) - y1( column ) )
+    return {
+        x: Math.sin( angle ) * radius,
+        y: Math.cos( angle ) * radius,
+        z: column
+    }
+} )
+
+
+
+// Animate Camera movement from file.
+;(async () => {
+    const cameraAnimationData = await (
+        fetch( document.head.baseURI + 'examples/assets/lcjs_example_0906_3dSimpleSurfaceMesh-camera.json' )
+            .then( r => r.json() )
+    )
+    if ( ! cameraAnimationData ) {
+        console.log(`No Camera animation data.`)
+        return
+    }
+    console.log(`Loaded Camera animation data.`)
+    let frame = 0
+    const nextFrame = () => {
+        if ( cameraAnimationEnabledCheckbox.getOn() ) {
+            const { cameraLocation } = cameraAnimationData.frames[Math.floor(frame) % cameraAnimationData.frames.length]
+            chart3D.setCameraLocation( cameraLocation )
+            frame += 1.5
+        }
+        requestAnimationFrame( nextFrame )
+    }
+    requestAnimationFrame( nextFrame )
+})()
+
+
+
+// * UI controls *
+const group = chart3D.addUIElement( UILayoutBuilders.Column
+    .setBackground( UIBackgrounds.Rectangle )
+)
+group
+    .setPosition( { x: 0, y: 100 } )
+    .setOrigin( UIOrigins.LeftTop )
+    .setMargin( 10 )
+    .setPadding( 4 )
+
+
+// Add UI for selecting surface style
+const options = []
+const addOption = ( label, onEnabled, defaultSelection = false ) => {
+    const checkBox = group.addElement( UIElementBuilders.CheckBox )
+        .setText( label )
+
+    if ( defaultSelection ) {
+        checkBox.setOn( true )
+        onEnabled()
+    }
+
+    checkBox.onSwitch( ( _, state ) => {
+        if ( state ) {
+            onEnabled()
+            checkBox.setMouseInteractions( false )
+            // Set all other check boxes off.
+            options.forEach( option => option.checkBox !== checkBox && option.checkBox.setOn( false ).setMouseInteractions( true ) )
+        }
+    } )
+
+    options.push( { checkBox } )
+}
+
+addOption( 'Color look up by Y', () =>
+    // Look up data point color from LUT by Y coordinate
+    surface.setFillStyle( new PalettedFill( { lut: paletteY, lookUpProperty: 'y' } ) )
+)
+addOption( 'Color look up by Value', () =>
+    // Look up data point color from LUT by number Value associated with it (assigned by user)
+    surface.setFillStyle( new PalettedFill( { lut: paletteValue, lookUpProperty: 'value' } ) )
+    , true
+)
+addOption( 'Individual Color', () =>
+    // Color data points by Colors assigned to each data point.
+    surface.setFillStyle( new IndividualPointFill()
+        // Specify Color to be used for data points that haven't been assigned a Color.
+        .setFallbackColor( ColorRGBA( 0, 255, 0 ) )
+    )
+)
+addOption( 'Solid color', () =>
+    // Single solid color.
+    surface.setFillStyle( new SolidFill( { color: ColorHSV( Math.random() * 360 ) } ) )
+)
+
+
+// Add UI for toggling wireframe.
+const handleWireframeToggled = ( state ) => {
+    // Set Wireframe style.
+    surface.setWireframeStyle( state ?
+        new SolidFill( { color: ColorRGBA( 0, 0, 0, 50 ) } ) :
+        emptyFill
+    )
+    wireframeCheckbox.setText( state ? 'Hide wireframe' : 'Show wireframe' )
+} 
+const wireframeCheckbox = group.addElement( UIElementBuilders.CheckBox )
+wireframeCheckbox.onSwitch((_, state) => handleWireframeToggled( state ))
+wireframeCheckbox.setOn( true )
+
+
+// Add UI control for toggling camera animation.
+const handleCameraAnimationToggled = ( state ) => {
+    cameraAnimationEnabledCheckbox.setText( state ? 'Disable camera animation' : 'Enable camera animation' )
+    if ( cameraAnimationEnabledCheckbox.getOn() !== state ) {
+        cameraAnimationEnabledCheckbox.setOn( state )
+    }
+}
+const cameraAnimationEnabledCheckbox = group.addElement( UIElementBuilders.CheckBox )
+cameraAnimationEnabledCheckbox.onSwitch((_, state) => handleCameraAnimationToggled( state ))
+handleCameraAnimationToggled( true )
